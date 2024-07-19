02ec0224be1ac1643b681400a069ce4f0fa73f8d
seria <seria.ati@gmail.com>
2024-07-19 14:15:11 +0900

    feat(agent-data): Add element and beta fields

--- zzz_asset_scrape/dotgg.py
+++ zzz_asset_scrape/dotgg.py
@@ -14,5 +14,7 @@ def get_agent_data() -> dict[str, Any]:
             "full_name": agent["fullName"],
             "icon_url": f"https://static.dotgg.gg/zenless/{agent['icon']}",
             "faction": agent["faction"],
+            "element": agent["element"][0],
+            "beta": True if agent["rarity"] == "0" else False,
         }
     return result
